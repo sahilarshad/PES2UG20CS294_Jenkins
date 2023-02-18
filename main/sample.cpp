c6d4a6f425f471b1b050095dc788aa058fde07aa
Sahil Arshad <sahil.arshad.a@gmail.com>
2023-02-18 11:52:08 +0530

    first commit

---
+++ main/sample.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    cout << "Task 5\n";
+    cout << "PES2UG20S294\n";
+    cout << "I have successfully built and run\n";
+    return 0;
+}
